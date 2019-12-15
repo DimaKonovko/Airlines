d6d660621f8793b19917508bb7cc0d2cf0c057ea
Dima Konovko <dima.konovko@mail.ru>
2019-12-15 12:59:24 +0300

    maven; Airlines were successfully moved to maven

---
+++ src/main/java/by/bsuir/konovko/airlines/bean/Passenger.java
@@ -0,0 +1,35 @@
+package by.bsuir.konovko.airlines.bean;
+
+import java.io.Serializable;
+
+public class Passenger implements Serializable {
+    // variables
+    private static int base_id = 0;
+    private int id;
+    private String fio;
+    private int age;
+
+
+
+    // methods
+    public int getId() { return id; }
+    public String getFio() { return fio; }
+    public int getAge() { return age; }
+
+    public void setFio(String fio) { this.fio = fio; }
+    public void setAge(int age) { this.age = age; }
+
+
+
+    public Passenger() {
+        base_id++;
+        this.id = base_id;
+    }
+
+    public Passenger(String fio, int age) {
+        base_id++;
+        this.id = base_id;
+        this.fio = fio;
+        this.age = age;
+    }
+}
